caad4a5e78d2a79c53c5fc4d5e0e6fb93efdc64d
CryptoProject0417 <whappyjunior@gmail.com>
2022-01-05 08:57:23 -0500

    upload gif

--- client/README.md
+++
@@ -1,1 +0,0 @@
-if you get any error related to sass so use node version v14.18.2
--- client/src/routes/routesPath.js
+++
@@ -1,5 +0,0 @@
-export const HUNTERS_VALLEY = '/';
-export const TRADING_POST = '/trading-post';
-export const INSTRUCTIONS = '/instructions';
-export const INVENTORY = '/inventory/:slug?';
-export const TRAINING_GROUND = '/training-ground';
